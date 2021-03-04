048d8849d23e9a2f76399ca4278c70c59ffca422
Gastonrt <gastonrt@gmail.com>
2021-03-04 13:18:06 -0300

    Proyecto Integrador Versión 1.6

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let cerrar = document.querySelectorAll(".close")[0];
+let abrir = document.querySelectorAll(".cta")[0];
+let modal = document.querySelectorAll(".modal")[0];
+let modalCr = document.querySelectorAll(".modal-container")[0];
+
+abrir.addEventListener("click", function(e){
+    e.preventDefault();
+    modalC.style.opacity = "1";
+    modalC.style.visibility = "visible";
+});
